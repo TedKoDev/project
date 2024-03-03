ac56c4bf3bc1c35bf6eeca8cf630642ca96dea3f
TEDKORDEV <ted.kordev@gmail.com>
2024-03-03 16:09:17 +0900

    navbar admin 처리

--- src/api/firebase.js
+++ src/api/firebase.js
@@ -1,36 +1,63 @@
-import { initializeApp } from 'firebase/app';
+import { initializeApp } from "firebase/app";
 import {
-    getAuth,
-    signInWithPopup,
-    GoogleAuthProvider,
-    signOut,
-    onAuthStateChanged,
-} from 'firebase/auth';
+  getAuth,
+  signInWithPopup,
+  GoogleAuthProvider,
+  signOut,
+  onAuthStateChanged,
+} from "firebase/auth";
+import { getDatabase, ref, child, get } from "firebase/database";
 
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.REACT_APP_FIREBASE_DB_URL,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DB_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth();
 const provider = new GoogleAuthProvider();
-
+const database = getDatabase(app);
 
 //로그인 로그아웃 함수 만들기
 export async function login() {
-    signInWithPopup(auth, provider).catch(console.error);
+  signInWithPopup(auth, provider).catch(console.error);
 }
 
 export async function logout() {
-    signOut(auth).catch(console.error);
+  signOut(auth).catch(console.error);
 }
 
 //로그인 상태 확인 함수 만들기
 export function onUserStateChange(callback) {
-    onAuthStateChanged(auth, (user) => {
-        callback(user);
+  onAuthStateChanged(auth, async (user) => {
+    // 1. 사용자가 있는경우( 로그인한 경우)
+    // 2. 어드민 인진 확인하기
+
+    const updatedUser = user ? await adminUser(user) : null;
+    callback(updatedUser);
+  });
+}
+
+async function adminUser(user) {
+  //2.사용자가 어디민 권한이 있는지 확인
+  //3. {...user, isAdmin: true/false} 전달
+
+  return get(ref(database, `admins`))
+    .then((snapshot) => {
+      if (snapshot.exists()) {
+        const admins = snapshot.val();
+        // console.log("admins", admins);
+
+        const isAdmin = admins.includes(user.uid);
+        return { ...user, isAdmin };
+        // return true;
+      } else {
+        return user;
+      }
+    })
+    .catch((error) => {
+      console.error(error);
     });
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,40 +1,35 @@
-import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-import { FiShoppingBag } from 'react-icons/fi';
-import { BsFillPencilFill } from 'react-icons/bs';
-import { login, logout, onUserStateChange } from '../api/firebase';
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { FiShoppingBag } from "react-icons/fi";
+import { BsFillPencilFill } from "react-icons/bs";
+import { login, logout, onUserStateChange } from "../api/firebase";
 
-
-import User from './User';
+import User from "./User";
 
 export default function Navbar() {
-
-
-  //상태를 만들어줘야함 
+  //상태를 만들어줘야함
   //로그인 상태를 확인해야함
 
   const [user, setUser] = useState();
-  console.log('user', user);
-
-
+  console.log("user", user);
 
   useEffect(() => {
-    onUserStateChange(setUser);
+    onUserStateChange((user) => {
+      console.log("user", user);
+      setUser(user);
+    });
   }, []);
 
-
-
-
   return (
-    <header className='flex justify-between border-b border-gray-300 p-2'>
-      <Link to='/' className='flex items-center text-4xl text-brand'>
+    <header className="flex justify-between border-b border-gray-300 p-2">
+      <Link to="/" className="flex items-center text-4xl text-brand">
         <FiShoppingBag />
         <h1>Shoppy</h1>
       </Link>
-      <nav className='flex items-center gap-4 font-semibold'>
-        <Link to='/products'>Products</Link>
-        <Link to='/carts'>Carts</Link>
-        <Link to='/products/new' className='text-2xl'>
+      <nav className="flex items-center gap-4 font-semibold">
+        <Link to="/products">Products</Link>
+        <Link to="/carts">Carts</Link>
+        <Link to="/products/new" className="text-2xl">
           <BsFillPencilFill />
         </Link>
         {user && <User user={user} />}
