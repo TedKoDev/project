8abcf84f340eb4b202205ba22e4f28dd63cd2aef
TED <ted.kordev@gmail.com>
2024-03-09 12:14:13 +0900

    add-css : ProductCard.jsx CSS

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import { AuthContextProvider } from "./components/context/AuthContext";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+
 
 function App() {
+
+  const queryClient = new QueryClient();
+
   return (
-    <>
+    <QueryClientProvider client={queryClient} >
       <AuthContextProvider>
         <Navbar />
         <Outlet />
       </AuthContextProvider>
-    </>
+    </QueryClientProvider>
   );
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Products from '../components/Products';
 
 export default function Home() {
     return (
-        <div>
-            Home
-        </div>
+        <>
+            <Products />
+        </>
     );
 }
