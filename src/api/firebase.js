14f8171abcfed341b53217bbc224f58f0eed815c
TED <ted.kordev@gmail.com>
2024-03-10 17:24:19 +0900

    10번째

--- src/api/firebase.js
+++ src/api/firebase.js
@@ -10,14 +10,11 @@ import {
 import { getDatabase, ref, set, get, remove } from "firebase/database";
 
 const firebaseConfig = {
-  // apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  // authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  // databaseURL: process.env.REACT_APP_FIREBASE_DB_URL,
-  // projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  apiKey: 'AIzaSyCeLt5FlkW-UJNhdjFmZVPT6ljyCYMTZh0',
-  authDomain: "hsquare-40253.firebaseapp.com",
-  databaseURL: "https://hsquare-40253-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "hsquare-40253",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DB_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+
 };
 
 const app = initializeApp(firebaseConfig);
